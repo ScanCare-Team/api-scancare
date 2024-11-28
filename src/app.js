ce87d353944392c6cfd88f0f234df7569a1597cb
Andi Batari <144105010+andibatari@users.noreply.github.com>
2024-11-28 22:14:12 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 //app.use('/api/scan', scanRoutes);
-
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+const HOST = 0.0.0.0;
+const PORT = 8000;
+app.listen(PORT,HOST, () => {
   console.log(`Server running on port ${PORT}`);
 });
