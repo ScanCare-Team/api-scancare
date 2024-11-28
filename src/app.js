82cb7c4fed910d6aa1fe64f377175212d152ed92
Asrini Muhsin <asrinimuhsin@gmail.com>
2024-11-29 00:10:08 +0800

    port 8080

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 //app.use('/api/scan', scanRoutes);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
