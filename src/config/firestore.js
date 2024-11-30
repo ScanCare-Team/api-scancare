c145ee0ce30ddaec34a02b9ae9f29eedab5bc0f7
Asrini Muhsin <asrinimuhsin@gmail.com>
2024-11-30 12:44:11 +0800

    file

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -4,11 +4,14 @@ require('dotenv').config();
 
 //const serviceAccountPath = path.join(__dirname, '..', 'config', 'project-scancare-3455aa2c9529.json');
 //const serviceAccountPath = '/secrets/service-account.json';
-const serviceAccountPath = process.env.FIREBASE_KEY_FILE_PATH || '/secrets/service-account.json';
+//const serviceAccountPath = process.env.FIREBASE_KEY_FILE_PATH
+const serviceAccountPath = path.resolve(process.env.FIREBASE_KEY_FILE_PATH);
+
 
 const db = new Firestore({
   projectId: process.env.FIREBASE_PROJECT_ID,
-  keyFilename: path.resolve(serviceAccountPath),
+  //keyFilename: path.resolve(serviceAccountPath),
+  keyFilename: serviceAccountPath,
 });
 
 db.listCollections()
